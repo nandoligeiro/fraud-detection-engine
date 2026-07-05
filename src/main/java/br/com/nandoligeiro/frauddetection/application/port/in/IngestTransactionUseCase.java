0a2c233a1f3caa0b7d28d51d1570d6f637c04b85
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 01:25:07 -0300

    refactor: simplify input port imports

--- src/main/java/br/com/nandoligeiro/frauddetection/application/port/in/IngestTransactionUseCase.java
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/port/in/IngestTransactionUseCase.java
@@ -1,8 +1,5 @@
 package br.com.nandoligeiro.frauddetection.application.port.in;
 
-import br.com.nandoligeiro.frauddetection.application.usecase.IngestTransactionCommand;
-import br.com.nandoligeiro.frauddetection.application.usecase.IngestionResult;
-
 public interface IngestTransactionUseCase {
 
     IngestionResult ingest(IngestTransactionCommand command);
