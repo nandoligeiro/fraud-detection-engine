7eae5ecd984c39e1c3e301d16a06aee4a3991ec6
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 14:12:16 -0300

    refactor: add rule id package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/domain/rule/model/RuleId.java
@@ -0,0 +1,18 @@
+package br.com.nandoligeiro.frauddetection.domain.rule.model;
+
+public record RuleId(String value) {
+    public RuleId {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("ruleId must not be blank");
+        }
+    }
+
+    public static RuleId of(String value) {
+        return new RuleId(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
