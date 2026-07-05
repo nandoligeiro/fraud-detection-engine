186a2e840a84825cf3e69fde5cd905d21305409e
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 12:27:09 -0300

    refactor: remove old transaction response package

--- src/main/java/br/com/nandoligeiro/frauddetection/adapter/in/rest/TransactionResponse.java
+++
@@ -1,9 +0,0 @@
-package br.com.nandoligeiro.frauddetection.adapter.in.rest;
-
-import br.com.nandoligeiro.frauddetection.application.port.in.IngestionStatus;
-
-public record TransactionResponse(
-        String transactionId,
-        IngestionStatus status
-) {
-}
