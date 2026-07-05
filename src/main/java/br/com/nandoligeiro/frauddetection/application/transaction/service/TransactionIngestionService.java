5afc28ef8b7aae07447e397f733e7d3444a584ec
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:46:27 -0300

    refactor: add transaction service package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/transaction/service/TransactionIngestionService.java
@@ -0,0 +1,7 @@
+package br.com.nandoligeiro.frauddetection.application.transaction.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionIngestionService {
+}
