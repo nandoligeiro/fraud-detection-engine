b99d82794a4c2a9890ce03a3304a3d4fffc11c92
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:16:40 -0300

    refactor: move detection result

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/detection/port/in/FraudDetectionResult.java
@@ -0,0 +1,26 @@
+package br.com.nandoligeiro.frauddetection.application.detection.port.in;
+
+import br.com.nandoligeiro.frauddetection.domain.fraud.model.FraudAlert;
+import br.com.nandoligeiro.frauddetection.domain.fraud.model.FraudDecision;
+import br.com.nandoligeiro.frauddetection.domain.transaction.model.Transaction;
+
+import java.util.Optional;
+
+public record FraudDetectionResult(
+        String transactionId,
+        FraudDecision decision,
+        Optional<FraudAlert> alert
+) {
+
+    public static FraudDetectionResult normal(Transaction transaction) {
+        return new FraudDetectionResult(transaction.id().value(), FraudDecision.NORMAL, Optional.empty());
+    }
+
+    public static FraudDetectionResult suspicious(Transaction transaction, FraudAlert alert) {
+        return new FraudDetectionResult(transaction.id().value(), FraudDecision.SUSPICIOUS, Optional.of(alert));
+    }
+
+    public boolean hasAlert() {
+        return alert.isPresent();
+    }
+}
