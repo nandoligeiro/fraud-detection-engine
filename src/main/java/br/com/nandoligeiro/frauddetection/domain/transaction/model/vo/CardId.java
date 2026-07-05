5e55cc47231e05aaf5feaf3e269d3434c2c1a5af
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 14:10:36 -0300

    refactor: add card id value object package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/domain/transaction/model/vo/CardId.java
@@ -0,0 +1,18 @@
+package br.com.nandoligeiro.frauddetection.domain.transaction.model.vo;
+
+public record CardId(String value) {
+    public CardId {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("cardId must not be blank");
+        }
+    }
+
+    public static CardId of(String value) {
+        return new CardId(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
