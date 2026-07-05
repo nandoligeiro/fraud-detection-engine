21f5815e71c67daa3c16f177b8d5e469607eb2b5
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 01:17:09 -0300

    feat: add logging transaction publisher adapter

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/adapter/out/logging/LoggingTransactionEventPublisherAdapter.java
@@ -0,0 +1,23 @@
+package br.com.nandoligeiro.frauddetection.adapter.out.logging;
+
+import br.com.nandoligeiro.frauddetection.application.port.out.TransactionEventPublisherPort;
+import br.com.nandoligeiro.frauddetection.domain.model.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggingTransactionEventPublisherAdapter implements TransactionEventPublisherPort {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingTransactionEventPublisherAdapter.class);
+
+    @Override
+    public void publish(Transaction transaction) {
+        log.info("transaction accepted for async processing transactionId={} accountId={} amount={} channel={}",
+                transaction.transactionId(),
+                transaction.accountId(),
+                transaction.amount(),
+                transaction.channel()
+        );
+    }
+}
