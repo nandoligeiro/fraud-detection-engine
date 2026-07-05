97a3df81cf764809648537d6f08b408437097c0e
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:16:47 -0300

    refactor: move detection output ports

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/detection/port/out/FraudAlertPublisherPort.java
@@ -0,0 +1,8 @@
+package br.com.nandoligeiro.frauddetection.application.detection.port.out;
+
+import br.com.nandoligeiro.frauddetection.domain.fraud.model.FraudAlert;
+
+public interface FraudAlertPublisherPort {
+
+    void publish(FraudAlert alert);
+}
