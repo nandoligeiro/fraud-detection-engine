9c68a5918749f4f0fdd3c4a50dfbff0c6c7faa16
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 01:15:23 -0300

    feat: add application bootstrap

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/FraudDetectionApplication.java
@@ -0,0 +1,12 @@
+package br.com.nandoligeiro.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectionApplication.class, args);
+    }
+}
