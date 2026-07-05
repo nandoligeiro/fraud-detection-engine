13b9183c139df2c11597c0a2fe0c6b2c6c88a813
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:17:09 -0300

    refactor: move transaction value objects

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/domain/transaction/model/vo/TransactionId.java
@@ -0,0 +1,19 @@
+package br.com.nandoligeiro.frauddetection.domain.transaction.model.vo;
+
+public record TransactionId(String value) {
+
+    public TransactionId {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("transactionId must not be blank");
+        }
+    }
+
+    public static TransactionId of(String value) {
+        return new TransactionId(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
