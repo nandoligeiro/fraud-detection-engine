2aaf021b764f11d4b222bc2f034bd366a817eb2d
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 12:25:05 -0300

    refactor: move alert logging adapter to infrastructure package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/infrastructure/adapter/out/logging/LoggingFraudAlertPublisherAdapter.java
@@ -0,0 +1,25 @@
+package br.com.nandoligeiro.frauddetection.infrastructure.adapter.out.logging;
+
+import br.com.nandoligeiro.frauddetection.application.port.out.FraudAlertPublisherPort;
+import br.com.nandoligeiro.frauddetection.domain.model.FraudAlert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "fraud.kafka.alert-publisher.enabled", havingValue = "false", matchIfMissing = true)
+public class LoggingFraudAlertPublisherAdapter implements FraudAlertPublisherPort {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingFraudAlertPublisherAdapter.class);
+
+    @Override
+    public void publish(FraudAlert alert) {
+        log.info("fraud alert ready for async publication alertId={} transactionId={} severity={} triggeredRules={}",
+                alert.alertId(),
+                alert.transactionId(),
+                alert.severity(),
+                alert.triggeredRules().size()
+        );
+    }
+}
