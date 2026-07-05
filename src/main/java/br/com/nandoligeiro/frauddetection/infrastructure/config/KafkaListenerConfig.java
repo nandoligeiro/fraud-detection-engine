f0aa9e10b58187c5bbc2c3ad19d3d71bb0892142
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 12:25:43 -0300

    refactor: move kafka listener config to infrastructure package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/infrastructure/config/KafkaListenerConfig.java
@@ -0,0 +1,11 @@
+package br.com.nandoligeiro.frauddetection.infrastructure.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+@ConditionalOnProperty(name = "fraud.kafka.enabled", havingValue = "true")
+public class KafkaListenerConfig {
+}
