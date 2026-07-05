1938794a7e94409e7e1df0ea9a06fa3612c67e0d
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:45:22 -0300

    refactor: add detection command in feature package

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/detection/port/in/EvaluateTransactionCommand.java
@@ -0,0 +1,23 @@
+package br.com.nandoligeiro.frauddetection.application.detection.port.in;
+
+import br.com.nandoligeiro.frauddetection.domain.model.Transaction;
+
+public final class EvaluateTransactionCommand {
+
+    private final Transaction transaction;
+
+    private EvaluateTransactionCommand(Transaction transaction) {
+        if (transaction == null) {
+            throw new IllegalArgumentException("transaction must not be null");
+        }
+        this.transaction = transaction;
+    }
+
+    public static EvaluateTransactionCommand of(Transaction transaction) {
+        return new EvaluateTransactionCommand(transaction);
+    }
+
+    public Transaction transaction() {
+        return transaction;
+    }
+}
