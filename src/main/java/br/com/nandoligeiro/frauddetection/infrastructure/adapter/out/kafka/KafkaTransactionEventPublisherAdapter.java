8fa6cb5e73d27ed684c51bd50c8dfdfc0c2a77ad
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:49:15 -0300

    refactor: use transaction feature publisher port in kafka adapter

--- src/main/java/br/com/nandoligeiro/frauddetection/infrastructure/adapter/out/kafka/KafkaTransactionEventPublisherAdapter.java
+++ src/main/java/br/com/nandoligeiro/frauddetection/infrastructure/adapter/out/kafka/KafkaTransactionEventPublisherAdapter.java
@@ -1,6 +1,6 @@
 package br.com.nandoligeiro.frauddetection.infrastructure.adapter.out.kafka;
 
-import br.com.nandoligeiro.frauddetection.application.port.out.TransactionEventPublisherPort;
+import br.com.nandoligeiro.frauddetection.application.transaction.port.out.TransactionEventPublisherPort;
 import br.com.nandoligeiro.frauddetection.domain.model.Transaction;
 import br.com.nandoligeiro.frauddetection.infrastructure.adapter.kafka.TransactionEventMapper;
 import br.com.nandoligeiro.frauddetection.infrastructure.adapter.kafka.TransactionEventPayload;
@@ -12,16 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 @ConditionalOnProperty(name = "fraud.kafka.enabled", havingValue = "true")
 public class KafkaTransactionEventPublisherAdapter implements TransactionEventPublisherPort {
-
     private final KafkaTemplate<String, TransactionEventPayload> kafkaTemplate;
     private final TransactionEventMapper mapper;
     private final String topic;
 
-    public KafkaTransactionEventPublisherAdapter(
-            KafkaTemplate<String, TransactionEventPayload> kafkaTemplate,
-            TransactionEventMapper mapper,
-            @Value("${fraud.kafka.topics.transaction-events}") String topic
-    ) {
+    public KafkaTransactionEventPublisherAdapter(KafkaTemplate<String, TransactionEventPayload> kafkaTemplate, TransactionEventMapper mapper, @Value("${fraud.kafka.topics.transaction-events}") String topic) {
         this.kafkaTemplate = kafkaTemplate;
         this.mapper = mapper;
         this.topic = topic;
