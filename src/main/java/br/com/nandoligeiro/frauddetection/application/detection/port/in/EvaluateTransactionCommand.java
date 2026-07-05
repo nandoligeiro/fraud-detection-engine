60822e289192a705636afb91c21b8a776fbe328f
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 13:16:31 -0300

    refactor: move evaluation command

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/detection/port/in/EvaluateTransactionCommand.java
@@ -0,0 +1,16 @@
+package br.com.nandoligeiro.frauddetection.application.detection.port.in;
+
+import br.com.nandoligeiro.frauddetection.domain.transaction.model.Transaction;
+
+import java.util.Objects;
+
+public record EvaluateTransactionCommand(Transaction transaction) {
+
+    public EvaluateTransactionCommand {
+        Objects.requireNonNull(transaction, "transaction must not be null");
+    }
+
+    public static EvaluateTransactionCommand of(Transaction transaction) {
+        return new EvaluateTransactionCommand(transaction);
+    }
+}
