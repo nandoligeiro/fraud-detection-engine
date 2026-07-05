d3428315ad853f3dcabc706656cf1367edec6203
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 01:16:37 -0300

    feat: add transaction ingestion service

---
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/service/TransactionIngestionService.java
@@ -0,0 +1,58 @@
+package br.com.nandoligeiro.frauddetection.application.service;
+
+import br.com.nandoligeiro.frauddetection.application.port.in.IngestTransactionUseCase;
+import br.com.nandoligeiro.frauddetection.application.port.out.IdempotencyStorePort;
+import br.com.nandoligeiro.frauddetection.application.port.out.TransactionEventPublisherPort;
+import br.com.nandoligeiro.frauddetection.application.usecase.IngestTransactionCommand;
+import br.com.nandoligeiro.frauddetection.application.usecase.IngestionResult;
+import br.com.nandoligeiro.frauddetection.domain.model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class TransactionIngestionService implements IngestTransactionUseCase {
+
+    private static final Duration IDEMPOTENCY_TTL = Duration.ofMinutes(10);
+
+    private final IdempotencyStorePort idempotencyStore;
+    private final TransactionEventPublisherPort transactionEventPublisher;
+
+    public TransactionIngestionService(
+            IdempotencyStorePort idempotencyStore,
+            TransactionEventPublisherPort transactionEventPublisher
+    ) {
+        this.idempotencyStore = idempotencyStore;
+        this.transactionEventPublisher = transactionEventPublisher;
+    }
+
+    @Override
+    public IngestionResult ingest(IngestTransactionCommand command) {
+        boolean firstProcessing = idempotencyStore.tryStartProcessing(command.transactionId(), IDEMPOTENCY_TTL);
+
+        if (!firstProcessing) {
+            return IngestionResult.duplicated(command.transactionId());
+        }
+
+        transactionEventPublisher.publish(toTransaction(command));
+        return IngestionResult.accepted(command.transactionId());
+    }
+
+    private Transaction toTransaction(IngestTransactionCommand command) {
+        return new Transaction(
+                command.transactionId(),
+                command.accountId(),
+                command.cardId(),
+                command.amount(),
+                command.currency(),
+                command.merchantId(),
+                command.merchantCategoryCode(),
+                command.channel(),
+                command.country(),
+                command.city(),
+                command.latitude(),
+                command.longitude(),
+                command.occurredAt()
+        );
+    }
+}
