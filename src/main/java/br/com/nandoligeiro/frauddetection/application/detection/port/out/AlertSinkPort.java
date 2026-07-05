9adf40ba236ee5e7fd8c193e909079612db50cdb
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 14:43:44 -0300

    refactor: remove unused alert sink port

--- src/main/java/br/com/nandoligeiro/frauddetection/application/detection/port/out/AlertSinkPort.java
+++
@@ -1,8 +0,0 @@
-package br.com.nandoligeiro.frauddetection.application.detection.port.out;
-
-import br.com.nandoligeiro.frauddetection.domain.model.FraudAlert;
-
-public interface AlertSinkPort {
-
-    void send(FraudAlert alert);
-}
