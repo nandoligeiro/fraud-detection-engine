1650b3e233959eca92f895e2e9a14d946235a0e5
Fernando Costa <nandoligeiro@gmail.com>
2026-07-05 16:06:22 -0300

    refactor: remove old transaction event publisher port

--- src/main/java/br/com/nandoligeiro/frauddetection/application/port/out/TransactionEventPublisherPort.java
+++
@@ -1,8 +0,0 @@
-package br.com.nandoligeiro.frauddetection.application.port.out;
-
-import br.com.nandoligeiro.frauddetection.domain.model.Transaction;
-
-public interface TransactionEventPublisherPort {
-
-    void publish(Transaction transaction);
-}
