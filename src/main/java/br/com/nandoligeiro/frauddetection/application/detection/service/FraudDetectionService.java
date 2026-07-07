62e1c1e1ed7a148ce2f24ab98791b5afd98bddf0
Fernando Costa <nandoligeiro@gmail.com>
2026-07-07 06:06:18 -0300

    chore: add detection flow logs

--- src/main/java/br/com/nandoligeiro/frauddetection/application/detection/service/FraudDetectionService.java
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/detection/service/FraudDetectionService.java
@@ -11,6 +11,8 @@ import br.com.nandoligeiro.frauddetection.domain.rule.model.FraudRule;
 import br.com.nandoligeiro.frauddetection.domain.rule.model.RuleEvaluationResult;
 import br.com.nandoligeiro.frauddetection.domain.rule.service.DeterministicRuleEngine;
 import br.com.nandoligeiro.frauddetection.domain.transaction.model.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
@@ -18,6 +20,8 @@ import java.util.List;
 
 @Service
 public class FraudDetectionService implements DetectFraudUseCase {
+
+    private static final Logger log = LoggerFactory.getLogger(FraudDetectionService.class);
 
     private final RuleProviderPort ruleProvider;
     private final FraudAlertPublisherPort alertPublisher;
@@ -34,18 +38,26 @@ public class FraudDetectionService implements DetectFraudUseCase {
         Transaction transaction = command.transaction();
         List<FraudRule> rules = ruleProvider.loadRules();
 
+        log.info("detection started transactionId={} accountId={} rulesLoaded={}", transaction.id().value(), transaction.accountId().value(), rules.size());
+
         if (rules.isEmpty()) {
+            log.info("detection finished transactionId={} decision=NORMAL reason=no-rules", transaction.id().value());
             return FraudDetectionResult.normal(transaction);
         }
 
         List<RuleEvaluationResult> triggeredRules = DeterministicRuleEngine.withRules(rules).evaluate(transaction);
 
         if (triggeredRules.isEmpty()) {
+            log.info("detection finished transactionId={} decision=NORMAL triggeredRules=0", transaction.id().value());
             return FraudDetectionResult.normal(transaction);
         }
 
         FraudAlert alert = alertFactory.createAlert(transaction, triggeredRules);
+        log.info("alert created alertId={} transactionId={} decision={} severity={} triggeredRules={}", alert.alertId(), alert.transactionId(), alert.decision(), alert.severity(), alert.triggeredRules().size());
+
         alertPublisher.publish(alert);
+
+        log.info("detection finished transactionId={} decision={} alertId={}", transaction.id().value(), alert.decision(), alert.alertId());
 
         return FraudDetectionResult.suspicious(transaction, alert);
     }
