a3f1694e6017487f1e16dcfe16714cfa4f65cc36
Fernando Costa <nandoligeiro@gmail.com>
2026-07-07 06:05:30 -0300

    chore: add transaction ingestion logs

--- src/main/java/br/com/nandoligeiro/frauddetection/application/transaction/service/TransactionIngestionService.java
+++ src/main/java/br/com/nandoligeiro/frauddetection/application/transaction/service/TransactionIngestionService.java
@@ -12,6 +12,8 @@ import br.com.nandoligeiro.frauddetection.domain.transaction.model.vo.GeoLocatio
 import br.com.nandoligeiro.frauddetection.domain.transaction.model.vo.Merchant;
 import br.com.nandoligeiro.frauddetection.domain.transaction.model.vo.Money;
 import br.com.nandoligeiro.frauddetection.domain.transaction.model.vo.TransactionId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -19,6 +21,7 @@ import java.time.Duration;
 @Service
 public class TransactionIngestionService implements TransactionIngestionUseCase {
 
+    private static final Logger log = LoggerFactory.getLogger(TransactionIngestionService.class);
     private static final Duration TTL = Duration.ofMinutes(10);
 
     private final TransactionProcessingGuardPort guard;
@@ -31,11 +34,18 @@ public class TransactionIngestionService implements TransactionIngestionUseCase 
 
     @Override
     public IngestionResult ingest(IngestTransactionCommand command) {
+        log.info("transaction ingestion started transactionId={} accountId={} amount={} currency={} channel={} country={}", command.transactionId(), command.accountId(), command.amount(), command.currency(), command.channel(), command.country());
+
         if (!guard.acquire(command.transactionId(), TTL)) {
+            log.info("transaction ingestion duplicated transactionId={} ttlSeconds={}", command.transactionId(), TTL.toSeconds());
             return IngestionResult.duplicated(command.transactionId());
         }
+
         Transaction transaction = toTransaction(command);
         publisher.publish(transaction);
+
+        log.info("transaction accepted for async processing transactionId={} accountId={} channel={} country={}", transaction.id().value(), transaction.accountId().value(), transaction.channel(), transaction.location().country());
+
         return IngestionResult.accepted(transaction.id().value());
     }
 
